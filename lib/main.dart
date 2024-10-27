83de5e9a3d9772d11a9957a0d7bae0e0e34d608e
JesseKiguta <j.kiguta@alustudent.com>
2024-10-27 21:12:14 +0200

    updated routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart'; // Import your welcome screen
-import 'screens/create_account_screen.dart';  // Import your sign-in screen
+import 'screens/create_account_screen.dart'; // Import your sign-in screen
+import 'screens/login_screen.dart';
+import 'screens/lessons.dart';
+import 'screens/art_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +13,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kulu Language App',
-      theme: ThemeData(
-        fontFamily: 'Poppins',  // Set Poppins as the default font
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: WelcomeScreen(),
-    );
+        title: 'Kulu Language App',
+        theme: ThemeData(
+          fontFamily: 'Poppins', // Set Poppins as the default font
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: "/",
+        routes: {
+          "/": (context) => WelcomeScreen(),
+          "/signup": (context) => CreateAccountScreen(),
+          "/login": (context) => LoginScreen(),
+          "/lessons": (context) => LanguageSelectionScreen(),
+          "/art": (context) => ArtScreen(),
+        });
   }
 }
